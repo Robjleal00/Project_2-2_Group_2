1e1e705b1c5c3b80b6728594a1263e9a1f46312a
Robert <R.J.Leal@student.tudelft.nl>
2022-02-18 15:47:30 +0100

    Created UI branch and added some UI code

--- src/main/java/org/openjfx/UI/MainApp.java
+++ src/main/java/org/openjfx/UI/MainApp.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 
 
@@ -19,6 +21,11 @@ public class MainApp extends Application {
         button.setOnAction(e -> System.out.println("Button is Pressed"));
 
         Scene scene = new Scene(button, 800,  600);
+
+        VBox pane = new VBox(10);
+        pane.setStyle("-fx-background-color: #383838");
+
+
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.setTitle("Surveillance Game");
