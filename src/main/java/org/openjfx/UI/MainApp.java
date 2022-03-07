c91145804f2d31d332759af10ebf08d2f597d01b
Robert <R.J.Leal@student.tudelft.nl>
2022-03-07 12:31:01 +0100

    Adding UI functionality for Launching App and Changing Scenes

--- src/main/java/org/openjfx/UI/MainApp.java
+++ src/main/java/org/openjfx/UI/MainApp.java
@@ -2,14 +2,21 @@ package org.openjfx.UI;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
-
-
-public class MainApp extends Application {
+public class MainApp extends Application implements EventHandler<ActionEvent>{
+    Button launchButton;
+    Scene launchScene, mainScene;
+    Label welcome;
+    Window window;
 
     public static void main(String[] args) {
         launch(args);
@@ -20,15 +27,39 @@ public class MainApp extends Application {
         Button button = new Button("Hello, World!");
         button.setOnAction(e -> System.out.println("Button is Pressed"));
 
-        Scene scene = new Scene(button, 800,  600);
+        window = primaryStage;
+
+        welcome = new Label("Welcome!");
+
+        launchButton = new Button("Launch Game");
+        launchButton.setOnAction(this);
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(launchButton);
+        layout.getChildren().add(welcome);
+
+        Scene scene = new Scene(button, 800, 600);
+
+        launchScene = new Scene(layout, 800, 600);
+
+        mainScene = new Scene(layout, 800, 600);
 
         VBox pane = new VBox(10);
         pane.setStyle("-fx-background-color: #383838");
 
 
         primaryStage.setScene(scene);
+        primaryStage.setScene(launchScene);
         primaryStage.setResizable(false);
         primaryStage.setTitle("Surveillance Game");
         primaryStage.show();
     }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if (event.getSource() == launchButton) {
+            System.out.println("Game Launched");
+        }
+    }
+
 }
