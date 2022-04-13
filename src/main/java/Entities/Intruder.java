745327b83a9877d5d2ce3dc0d5081e76c229aa01
Cloud956 <pmlewandowski@gmail.com>
2022-04-13 11:11:21 +0200

    not used

---
+++ src/main/java/Entities/Intruder.java
@@ -0,0 +1,46 @@
+package Entities;
+
+import Enums.EntityType;
+import Enums.Moves;
+
+public class Intruder extends Entity{
+    @Override
+    public EntityType getType() {
+        return super.getType();
+    }
+
+    @Override
+    public Moves getMove() {
+        return super.getMove();
+    }
+
+    @Override
+    public void turnLeft() {
+        super.turnLeft();
+    }
+
+    @Override
+    public void turnRight() {
+        super.turnRight();
+    }
+
+    @Override
+    public void turnAround() {
+        super.turnAround();
+    }
+
+    @Override
+    public void walk(int d) {
+        super.walk(d);
+    }
+
+    @Override
+    public void showMeWhatUSaw() {
+        super.showMeWhatUSaw();
+    }
+
+    @Override
+    public void setPosition(int[] xy) {
+        super.setPosition(xy);
+    }
+}
