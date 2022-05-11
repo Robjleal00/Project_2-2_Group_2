372b72c13f3042cb1d54fa8adebcecd063e3e1b7
Robert <R.J.Leal@student.tudelft.nl>
2022-05-11 13:07:38 +0200

    testing with main method

--- src/main/java/Chasing/Node.java
+++ src/main/java/Chasing/Node.java
@@ -117,6 +117,35 @@ public class Node implements Comparable<Node> {
 
     }
 
+    public static void main(String[] args) {
+        Node head = new Node(3);
+        head.g = 0;
+
+        Node n1 = new Node(2);
+        Node n2 = new Node(2);
+        Node n3 = new Node(2);
+
+        head.addBranch(1, n1);
+        head.addBranch(5, n2);
+        head.addBranch(2, n3);
+        n3.addBranch(1, n2);
+
+        Node n4 = new Node(1);
+        Node n5 = new Node(1);
+
+        Node target = new Node(0);
+
+        n1.addBranch(7, n4);
+        n2.addBranch(4, n5);
+        n3.addBranch(6, n4);
+
+        n4.addBranch(3, target);
+        n5.addBranch(1, n4);
+        n5.addBranch(3, target);
+
+        Node res = aStar(head, target);
+        printPath(res);
+    }
 }
 
 
