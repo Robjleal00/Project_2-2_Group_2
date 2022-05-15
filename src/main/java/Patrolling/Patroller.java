f21b65394a15510f1c4af7af53a94b8c06891e82
Kaiwei Ni <kaiw@live.fr>
2022-05-15 18:45:49 +0200

    Fluff

--- src/main/java/Patrolling/Patroller.java
+++ src/main/java/Patrolling/Patroller.java
@@ -4,25 +4,43 @@ import Config.Variables;
 import Enums.Moves;
 import Enums.Rotations;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+
 public class Patroller {
     private int maxDepth;
     public int dfsRecursive(int depth, int[] xy, Moves availableMoves, int[][] lastSeen, Rotations rotation)
     {
-        int maxValue = 0;
-        if(depth == 0)
+
+        if(depth != 0)
         {
-            //Reached the end of the DFS, return the value of current child back up so that the parent can decide
-            //which has the highest value
+            int maxValue = 0;
+            //Create an array list to keep track of all our values
+            ArrayList<int> nodeValues = new ArrayList<>();
+
+            // Store the values of all possibilities and return the highest one
+            for(availableMoves : availableMoves)
+            {
+                Rotations newRotation = null;
+                int nodeValue = dfsRecursive(depth - 1, xy ,getChildrensMoves(xy, lastSeen, rotation), lastSeen, newRotation);
+                nodeValues.add(nodeValue);
+            }
+            for(int i = 0; i < nodeValues.size(); i++)
+            {
+                if(maxValue < nodeValues.get(i))
+                {
+                    maxValue = nodeValues.get(i);
+                }
+            }
             return maxValue;
         }
         else
         {
-            //Return the highest value of all the children
-            for(availableMoves : availableMoves)
-            {
-                Rotations newRotation = null;
-                dfsRecursive(depth - 1, xy ,getChildrensMoves(xy, lastSeen, rotation), lastSeen, newRotation);
-            }
+            //Reached the end of the DFS, return the value of current child back up so that the parent can decide
+            //which has the highest value
+            return maxValue;
         }
         return maxValue;
     }
@@ -40,6 +58,55 @@ public class Patroller {
         return null;
     }
 
+    //Returns the SUM of all seen squares
+    public int getValue(int[][] lastSeen, Variables vr, int[] xy, Rotations rotation)
+    {
+        int value = 0;
+        int range = vr.eyeRange();
+        switch(rotation){
+            case BACK -> {
+                for(int i = xy[0] - 1; i < xy[0] + 1; i++){
+                    for(int j = xy[1]; j < xy[1] + range; j++){
+                        value  += lastSeen[i][j];
+                    }
+                }
+            }
+            case FORWARD -> {
+                for(int i = xy[0] - 1; i < xy[0] + 1; i++){
+                    for(int j = xy[1] - range; j < xy[1] ; j++){
+                        value  += lastSeen[i][j];
+                    }
+
+                }
+            }
+            case LEFT -> {
+                for(int i = xy[0] - range; i < xy[0]; i++){
+                    for(int j = xy[1] - 1; j < xy[1] + 1; j++){
+                        value  += lastSeen[i][j];
+                    }
+                }
+            }
+            case RIGHT ->{
+                for(int i = xy[0]; i < xy[0] + range; i++){
+                    for(int j = xy[1] - 1; j < xy[1] + 1; j++){
+                        value  += lastSeen[i][j];
+                    }
+                }
+            }
+        }
+        return value;
+    }
+
+    /**
+     *        Creates a new copy of lastSeen to:
+     *          A) be used to get the value
+     *          B) increment unseen squares in the next iteration for this specific child node
+     * @param lastSeen
+     * @param vr
+     * @param xy
+     * @param rotation
+     * @return
+     */
     public int[][] setSeen(int[][] lastSeen, Variables vr, int []xy, Rotations rotation)
     {
         int[][] newLastSeen = new int[lastSeen.length][lastSeen[0].length];
