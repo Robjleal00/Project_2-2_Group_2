f814ae95a569621dc6687dadf1cc1c2a93fd6282
ashademeij <a.demeij@student.maastrichtuniversity.nl>
2022-06-13 16:00:49 +0200

    start B&M

--- src/main/java/BrickAndMortar/BrickMortar.java
+++ src/main/java/BrickAndMortar/BrickMortar.java
@@ -1,5 +1,6 @@
 package BrickAndMortar;
 
+import Enums.Moves;
 import Enums.Rotations;
 
 public class BrickMortar {
@@ -8,8 +9,10 @@ public class BrickMortar {
     // 1 = Explored
     // 2 = Visited
     // 3 = Wall
-    private int[][] map;
-    private int[] xy;
+
+
+    private int [][]map;
+    private int []xy;
 
     private boolean exploDone;
     private final double randomness = 0.2;
@@ -17,16 +20,18 @@ public class BrickMortar {
 
 
 
-
     public BrickMortar(int[][] map, Rotations rot){
-      this.map = map;
-      this.exploDone = false;
-      this.rot = rot;
+        this.map = map;
+        this.exploDone = false;
+        this.rot = rot;
     }
 
 
     public Moves brickAndMortar()
     {
 
+        return null;
     }
+
+
 }
