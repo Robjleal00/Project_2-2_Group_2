7a76ad63b10aeec3adbbe6095ebe7ca4d3be8e31
Robert <R.J.Leal@student.tudelft.nl>
2022-06-15 13:06:32 +0200

    Direction enums added for QLearning

---
+++ src/main/java/QLearning/Direction.java
@@ -0,0 +1,25 @@
+package QLearning;
+
+/**
+ * Enum containing possible directions player can take.
+ *
+ * @author mirza
+ */
+public enum Direction {
+    /**
+     * Player will move up.
+     */
+    UP,
+    /**
+     * Player will move right.
+     */
+    RIGHT,
+    /**
+     * Player will move down.
+     */
+    DOWN,
+    /**
+     * Player will move left.
+     */
+    LEFT
+}
